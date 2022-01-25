feade4d642ad11e6ba422034c2ee21c244d89b42
Ammar96399 <50889372+Ammar96399@users.noreply.github.com>
2022-01-25 11:39:08 +0100

    SideBar

--- src/Components_remake/SideBar.tsx
+++ src/Components_remake/SideBar.tsx
@@ -1,13 +1,57 @@
-import {faBars} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native';
 
 const windowWidth = Dimensions.get('window').width;
 
 function SideBar() {
   return (
     <View style={styles.box}>
-      <View style={{flex: 1, width: '90%', backgroundColor: 'red'}}>
+      <Button title="home">
+        <Text>Home</Text>
+      </Button>
+      <Button title="validate">
+        <Text>Validate</Text>
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: windowWidth - windowWidth / 1.07,
+    height: '100%',
+    paddingVertical: 25,
+  },
+});
+export default SideBar;
+
+/*
+ <View
+        style={{
+          flex: 1,
+          width: '90%',
+          height: '10%',
+          backgroundColor: 'red',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <FontAwesomeIcon
+          icon={faBars}
+          style={{padding: 4, color: 'white'}}
+          size={40}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '90%',
+          backgroundColor: 'green',
+        }}>
         <FontAwesomeIcon icon={faBars} style={{padding: 4, color: 'white'}} />
       </View>
       <View
@@ -20,18 +64,4 @@ function SideBar() {
         }}>
         <FontAwesomeIcon icon={faBars} style={{padding: 4, color: 'white'}} />
       </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: windowWidth - windowWidth / 1.07,
-    paddingVertical: 25,
-  },
-});
-export default SideBar;
+*/
