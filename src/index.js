fd6f318e81f2527d045eef633b7a909c60e98996
Thibault Le Goff <thib.legoff@gmail.com>
2022-03-08 22:06:21 +0100

    Change bracket spacing rule

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * When compiling for web, it will search for index.js on root/src.
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import reportWebVitals from './reportWebVitals';
 
 // RNVI integration for web
