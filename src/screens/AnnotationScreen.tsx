7a79599c06169fe6d269e23b28206bcfd756198b
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2022-03-09 15:43:06 +0100

    Navigation

--- src/screens/AnnotationScreen.tsx
+++ src/screens/AnnotationScreen.tsx
@@ -1,43 +1,61 @@
 /* eslint-disable react-native/no-inline-styles */
-import {Button, Dimensions, StyleSheet, View} from 'react-native';
-import {Shadow} from 'react-native-shadow-2';
+import { Button, Dimensions, StyleSheet, View } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
 import LettersMenu from '../Components_remake/LettersMenu';
 import SideBar from '../Components_remake/SideBar';
 import Word from '../Components_remake/Word';
 
 const windowWidth = Dimensions.get('window').width;
 
-function AnnotationScreen() {
-  return (
-    <View style={styles.screen}>
-      <SideBar />
-      <Shadow containerViewStyle={{alignSelf: 'flex-end'}}>
-        <View style={styles.annotation}>
-          <LettersMenu />
-          <Word />
-        </View>
-      </Shadow>
-    </View>
-  );
+interface AnnotationScreenProps {
+    navigation: any;
+}
+
+
+function AnnotationScreen(props: AnnotationScreenProps) {
+
+    const fileScreen = () => { console.log("yes"); props.navigation.navigate('FileSelectionScreen'); }
+
+    return (
+        <View style={styles.screen}>
+            <SideBar />
+            <Shadow containerViewStyle={{ alignSelf: 'flex-end' }}>
+                <View style={styles.annotation}>
+                    <View style={styles.home}>
+                        <Button title="Menu"
+                            onPress={() => fileScreen()}>
+                        </Button>
+                    </View>
+                    <LettersMenu />
+                    <Word />
+                </View>
+            </Shadow >
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
-  annotation: {
-    flex: 1,
-    width: windowWidth / 1.07,
-    backgroundColor: '#e1e2e1',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    borderRadius: 40,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  screen: {
-    height: '100%',
-    width: '100%',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
+    annotation: {
+        flex: 1,
+        width: windowWidth / 1.07,
+        backgroundColor: '#e1e2e1',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        borderRadius: 40,
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    screen: {
+        height: '100%',
+        width: '100%',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    home: {
+        position: 'absolute',
+        right: 30,
+        top: 30,
+    },
 });
 export default AnnotationScreen;
