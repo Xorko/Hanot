5dcfeb42eaec056862cf740830bd8651202ee409
Agogue Benoit <benoit.agogue@etudiant.univ-rennes1.fr>
2022-04-15 19:28:23 +0200

    Remove the changeTrueImageSize function from the context

--- src/screens/annotation/image-annotation-screen/context/TrueImageSizeContext.ts
+++ src/screens/annotation/image-annotation-screen/context/TrueImageSizeContext.ts
@@ -3,10 +3,8 @@ import {Size} from '../types/image-annotation-types';
 
 type TrueImageSizeContextValue = {
   trueImageSize?: Size;
-  changeTrueImageSize: (size: Size) => void;
 };
 
 export const TrueImageSizeContext = createContext<TrueImageSizeContextValue>({
   trueImageSize: {} as Size,
-  changeTrueImageSize: () => {},
 });
