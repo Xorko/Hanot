b2294bd8eba544f9dfe3ea991209f1b920fb21d0
Agogue Benoit <benoit.agogue@etudiant.univ-rennes1.fr>
2022-04-15 15:19:20 +0200

    Adjust size of crops to the size of their container

--- src/screens/annotation/image-annotation-screen/components/Crop.tsx
+++ src/screens/annotation/image-annotation-screen/components/Crop.tsx
@@ -1,23 +1,19 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Canvas, {Image as CanvasImage} from 'react-native-canvas';
 import {useAppSelector} from '../../../../app/hooks';
-import {DisplayedImageSizeContext} from '../context/DisplayedImageSizeContext';
 import {CurrentAnnotatedImageState} from '../current-annotated-image';
-import {Point} from '../types/image-annotation-types';
+import {Point, Size} from '../types/image-annotation-types';
 import {getExtemityOfPath} from '../utils/crop-utils';
 
 interface CropPropsType {
   path: Point[];
+  size: Size;
 }
 
-const Crop = ({path}: CropPropsType) => {
+const Crop = ({path, size}: CropPropsType) => {
   const imageSrc = useAppSelector(
     (state: {currentAnnotatedImage: CurrentAnnotatedImageState}) =>
       state.currentAnnotatedImage.annotatedImage.imageSource,
-  );
-
-  const {displayedImageSize: imageDisplayedSize} = useContext(
-    DisplayedImageSizeContext,
   );
 
   const [currentPath, setCurrentPath] = useState<Point[]>([]);
@@ -28,8 +24,7 @@ const Crop = ({path}: CropPropsType) => {
   }, []);
 
   const handleCanvas = (canvas: Canvas) => {
-    // TODO : find a way to calculate only on first render
-    if (canvas && (path !== currentPath || firstRender) && imageDisplayedSize) {
+    if (canvas && (path !== currentPath || firstRender)) {
       const {minX, minY, maxX, maxY} = getExtemityOfPath(path);
       const [width, height] = [maxX - minX, maxY - minY];
       canvas.width = width;
@@ -56,13 +51,7 @@ const Crop = ({path}: CropPropsType) => {
         croppedImagePos.slice(1).forEach(({x, y}) => context.lineTo(x, y));
         context.lineTo(croppedImagePos[0].x, croppedImagePos[0].y);
         context.clip();
-        context.drawImage(
-          imageObj,
-          -minX,
-          -minY,
-          imageDisplayedSize.width,
-          imageDisplayedSize.height,
-        );
+        context.drawImage(imageObj, -minX, -minY, size.width, size.height);
         setCurrentPath(path);
       });
     }
