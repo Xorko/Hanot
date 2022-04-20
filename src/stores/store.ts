fcf5551a4bb24824e52a2f5c4a65ca52d88c8bfa
Agogue <agoguebenoit@gmail.com>
2022-04-20 23:17:51 +0200

    feat: :sparkles: add slice to store annotated image

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currentAnnotatedImageReducer from '../screens/annotation/image-annotation-screen/current-annotated-image';
-import currentWordReducer from '../screens/annotation/inkml-annotation-screen/currentWordSlice';
+import currentWordReducer from '../screens/annotation/inkml-annotation-screen/current-word-slice';
 import loadedFilesReducer from '../screens/file-selection-screen/loaded-files-slice';
+import annotatedImagesReducer from '../shared/annotated-image-files';
 
 export const store = configureStore({
   reducer: {
     loadedFiles: loadedFilesReducer,
     currentAnnotatedImage: currentAnnotatedImageReducer,
     currentWord: currentWordReducer,
+    annotatedImages: annotatedImagesReducer,
   },
 });
 
