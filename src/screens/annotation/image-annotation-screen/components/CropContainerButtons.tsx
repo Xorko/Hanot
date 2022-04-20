4e7102f92268aacfd75dbaa3eb598580b8c36a6b
Agogue <agoguebenoit@gmail.com>
2022-04-20 23:15:39 +0200

    fix(image-selection-screen): :adhesive_bandage: fix the crop deletion to remove annotation from its pixels in the store

--- src/screens/annotation/image-annotation-screen/components/CropContainerButtons.tsx
+++ src/screens/annotation/image-annotation-screen/components/CropContainerButtons.tsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import { View } from 'react-native';
-import { useAppDispatch } from '../../../../stores/hooks';
+import { useAppDispatch, useAppSelector } from '../../../../stores/hooks';
 import { useCurrentSelectedCropContext } from '../context/CurrentSelectedCropContext';
-import { currentAnnotatedImageRemoveCrop } from '../current-annotated-image';
+import { useTrueImageSizeContext } from '../context/TrueImageSizeContext';
+import {
+  currentAnnotatedImageRemoveCrop,
+  setCurrentAnnotatedImagePixels,
+} from '../current-annotated-image';
+import { Pixel } from '../types/image-annotation-types';
+import { getAllPointsInPath } from '../utils/pixels-utils';
 import DeleteButton from './DeleteButton';
 import ValidateButton from './ValidateButton';
+const cloneDeep = require('clone-deep');
 
 type CropContainerButtonsPropsType = {
-  currentSelectedCrop?: number;
   annotate: () => void;
 };
 
-const CropContainerButtons = ({
-  currentSelectedCrop,
-  annotate,
-}: CropContainerButtonsPropsType) => {
+const CropContainerButtons = ({ annotate }: CropContainerButtonsPropsType) => {
   //===========================================================================
   // Redux
   //===========================================================================
 
   const dispatch = useAppDispatch();
 
+  const currentImage = useAppSelector(
+    state => state.currentAnnotatedImage.annotatedImage,
+  );
+
   //===========================================================================
   // Contexts
   //===========================================================================
 
-  const { setCurrentSelectedCrop } = useCurrentSelectedCropContext();
+  const { currentSelectedCrop, setCurrentSelectedCrop } =
+    useCurrentSelectedCropContext();
+  const { trueImageSize } = useTrueImageSizeContext();
 
   //===========================================================================
   // Functions
@@ -40,8 +49,18 @@ const CropContainerButtons = ({
    * Removes the crop from the redux store
    */
   const deleteCrop = () => {
-    if (currentSelectedCrop !== undefined) {
+    if (currentSelectedCrop !== undefined && trueImageSize) {
+      const pixelsCopy: Pixel[] = cloneDeep(currentImage.imagePixels);
+      getAllPointsInPath(
+        currentImage.imageCrops[currentSelectedCrop].cropPath,
+        trueImageSize.width,
+      ).forEach((index: number) => {
+        // For every point, sets the annotation of the corresponding pixel to the one of the crop, if the pixel is not white (background)
+        const pixel: Pixel = pixelsCopy[index];
+        pixel.annotation = undefined;
+      });
       dispatch(currentAnnotatedImageRemoveCrop(currentSelectedCrop));
+      dispatch(setCurrentAnnotatedImagePixels(pixelsCopy));
       setCurrentSelectedCrop(undefined);
     }
   };
