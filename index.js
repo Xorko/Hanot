bafb9037d8b6a5796ed29e1fd31313d832aa70ee
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-21 18:03:19 +0200

    chore: :art: prettify code

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * When compiling for mobile, it will search for index.js on root.
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import App from './src/App';
-import {name as appName} from './src/app.json';
+import { name as appName } from './src/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
