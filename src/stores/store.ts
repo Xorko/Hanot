28b84e4d51c107efa9d0fd8dcb047af3c00a4bef
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-21 23:05:26 +0200

    fix: :bug: correct bad import

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import currentAnnotatedImageReducer from '../screens/annotation/image-annotation-screen/current-annotated-image';
 import currentWordReducer from '../screens/annotation/inkml-annotation-screen/current-word-slice';
 import loadedFilesReducer from '../screens/file-selection-screen/loaded-files-slice';
-import annotatedImagesReducer from '../shared/annotated-image-files';
+import annotatedImagesReducer from '../shared/annotated-image-files-slice';
 
 export const store = configureStore({
   reducer: {
