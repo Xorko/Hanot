3dfa61a9f0bd9d02cdc898be60a8ea4e427cfe06
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-21 23:36:17 +0200

    fix: :ambulance: migrate off RNGHEnabledRootView
    The app was crashing at startup on android

--- android/app/src/main/java/com/hanot/MainActivity.java
+++ android/app/src/main/java/com/hanot/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,16 +14,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Hanot";
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 
   /**
--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -1,4 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../stores/store';
 
@@ -9,9 +11,19 @@ interface AppProvidersProps {
 function AppProviders({ children }: AppProvidersProps) {
   return (
     <ReduxProvider store={store}>
-      <NavigationContainer>{children}</NavigationContainer>
+      <NavigationContainer>
+        <GestureHandlerRootView style={styles.ghrv}>
+          {children}
+        </GestureHandlerRootView>
+      </NavigationContainer>
     </ReduxProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  ghrv: {
+    flex: 1,
+  },
+});
 
 export default AppProviders;
