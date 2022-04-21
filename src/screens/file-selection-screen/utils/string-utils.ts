a3a84ac8fe2b5b9e10e48eef099257673f5c8472
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-22 01:27:35 +0200

    fix: :bug: fix invalid condition
    It was limiting the number of characters when str.length = n

--- src/screens/file-selection-screen/utils/string-utils.ts
+++ src/screens/file-selection-screen/utils/string-utils.ts
@@ -1,5 +1,5 @@
 export const limitStringLength = (str: string, n: number) => {
-  if (str.length >= n) {
+  if (str.length > n) {
     return `${str.split('').splice(0, n).join('')}...`;
   }
   return str;
