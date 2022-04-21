83002ad46dc4a810186a5399ccdd13467bfee7cf
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-22 01:32:59 +0200

    docs: :memo: add documentation for limitStringLength

--- src/screens/file-selection-screen/utils/string-utils.ts
+++ src/screens/file-selection-screen/utils/string-utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Limits the number of characters in a string.
+ *
+ * @param str The string to limit.
+ * @param n The number of characters to limit the string to.
+ * @returns The original string if it is less than or equal to the limit, otherwise the limited string followed by `...`.
+ */
 export const limitStringLength = (str: string, n: number) => {
   if (str.length > n) {
     return `${str.split('').splice(0, n).join('')}...`;
