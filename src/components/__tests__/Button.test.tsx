a89e70366e96d074d3a7f30d207d226deb87b26a
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-24 12:28:13 +0200

    test(button): :white_check_mark: add tests for button

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render } from 'test-utils';
+import colors from '../../style/colors';
+import Button from '../Button';
+
+test('it should call the onPress function when pressed', () => {
+  const onPress = jest.fn();
+  const { getByTestId } = render(
+    <Button variant="primary" title="test" onPress={onPress} />,
+  );
+
+  fireEvent(getByTestId('btn'), 'press');
+
+  expect(onPress).toHaveBeenCalledTimes(1);
+});
+
+test('renders with the correct background color', () => {
+  const { getByTestId, rerender } = render(
+    <Button variant="primary" title="test" />,
+  );
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.primary });
+
+  rerender(<Button variant="secondary" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.secondary });
+
+  rerender(<Button variant="success" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.success });
+
+  rerender(<Button variant="danger" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.danger });
+
+  rerender(<Button variant="warning" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.warning });
+
+  rerender(<Button variant="info" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.info });
+
+  rerender(<Button variant="light" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.light });
+
+  rerender(<Button variant="dark" title="test" />);
+  expect(getByTestId('btn')).toHaveStyle({ backgroundColor: colors.dark });
+});
