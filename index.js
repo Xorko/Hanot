dd49aac361e8140f551e94946c97c38467b2e6a7
Agogue <agoguebenoit@gmail.com>
2022-04-25 13:15:14 +0200

    chore: :package: add nanoid and react-native-get-random-values required for nanoid

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import App from './src/App';
 import { name as appName } from './src/app.json';
+import 'react-native-get-random-values';
 
 AppRegistry.registerComponent(appName, () => App);
