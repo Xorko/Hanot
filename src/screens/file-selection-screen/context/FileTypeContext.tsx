7318b5e2e7303710dcd534524764d1b94f197f67
Agogue <agoguebenoit@gmail.com>
2022-04-28 20:12:29 +0200

    refactor: :truck: rename and move file-types

--- src/screens/file-selection-screen/context/FileTypeContext.tsx
+++ src/screens/file-selection-screen/context/FileTypeContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react';
-import { FileType } from '../types/files-type';
+import { FileType } from '../../../types/file-types';
 
 type FileTypeContextValue = {
   fileType: FileType;
