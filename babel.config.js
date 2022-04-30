a5450a3369e0ba85931169e57809de39fc29b92c
Thibault Le Goff <thib.legoff@gmail.com>
2022-04-30 20:41:36 +0200

    chore: :arrow_up: upgrade reanimated to 2.3
    This upgrade provides layout animation

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset', {useTransformReactJSXExperimental: true}]],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      { useTransformReactJSXExperimental: true },
+    ],
+  ],
   plugins: [
     [
       '@babel/plugin-transform-react-jsx',
       {
-        runtime: 'automatic'
-      }
-    ]
-  ]
+        runtime: 'automatic',
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
